d74a229bd4519983665ffd194fd09c898b69da70
Corey Gomon <cgomon@gmail.com>
2020-05-29 19:27:40 -0400

    test

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
 #
 # Exercise 1.5
+
+def bounce_ball(num_bounces, init_height, elasticity):
+    height = init_height
+    for i in range(num_bounces):
+        print(i+1, round(height, 4))
+        height *= elasticity
